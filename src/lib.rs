28e73d71c82de6d3ce607ba9a7a4d1497753e127
Alexander Liesenfeld <alexander.liesenfeld@outlook.com>
2020-09-27 14:52:12 +0200

    Cleaning up

--- src/lib.rs
+++ src/lib.rs
@@ -1440,12 +1440,3 @@ lazy_static! {
     static ref REMOTE_SERVER_POOL_REF: Arc<Pool<Arc<dyn MockServerAdapter + Send + Sync>>> =
         Arc::new(Pool::new(1));
 }
-
-
-pub fn mock<'a, F>(config_fn: F) -> MockRef<'a>
-    where
-        F: FnOnce(When, Then),
-{
-    let ms = MockServer::start();
-    return Mock::new().create_on(&ms);
-}
