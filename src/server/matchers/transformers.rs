04336f32e5fbc68e23946f997d42ce468eb8a59e
Alexander Liesenfeld <alexander.liesenfeld@outlook.com>
2020-10-07 21:51:38 +0200

    New test cases

--- src/server/matchers/transformers.rs
+++ src/server/matchers/transformers.rs
@@ -37,3 +37,50 @@ impl Transformer<String, String> for ToLowercaseTransformer {
         Ok(v.to_lowercase())
     }
 }
+
+
+#[cfg(test)]
+mod test {
+    use crate::server::matchers::transformers::{DecodeBase64ValueTransformer, Transformer, ToLowercaseTransformer};
+
+    #[test]
+    fn base64_decode_transformer() {
+        // Arrange
+        let transformer = DecodeBase64ValueTransformer::new();
+
+        // Act
+        let result = transformer.transform(&"dGVzdA==".to_string());
+
+        // Assert
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap(), "test".to_string());
+    }
+
+
+    #[test]
+    fn base64_decode_transformer_error() {
+        // Arrange
+        let transformer = DecodeBase64ValueTransformer::new();
+
+        // Act
+        let result = transformer.transform(&"xÿ".to_string());
+
+        // Assert
+        assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn to_lowercase_transformer() {
+        // Arrange
+        let transformer = ToLowercaseTransformer::new();
+
+        // Act
+        let result = transformer.transform(&"HeLlO".to_string());
+
+        // Assert
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap(), "hello".to_string());
+    }
+
+}
+
