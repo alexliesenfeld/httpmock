5b306d1f57e0a05951a6ea1cc03491d53d7bba30
Alexander Liesenfeld <alexander.liesenfeld@outlook.com>
2021-03-12 01:32:18 +0100

    Removed draft test

--- tests/examples/delay_tests.rs
+++ tests/examples/delay_tests.rs
@@ -27,26 +27,3 @@ fn delay_test() {
     assert_eq!(response.status(), 200);
     assert_eq!(start_time.elapsed().unwrap() > delay, true);
 }
-
-/*
-#[test]
-fn delay_tests() {
-    // Arrange
-    let start_time = SystemTime::now();
-
-    let server = MockServer::start();
-
-    let mock = server.mock(|when, then| {
-        when.path("/delay");
-        then.status(200);
-    });
-
-    // Act: Send the HTTP request
-    let response = get(server.url("/delayx")).unwrap();
-    let response = get(server.url("/delay")).unwrap();
-
-    // Assert
-    mock.assert_hits(2);
-    assert_eq!(response.status(), 200);
-}
-*/
