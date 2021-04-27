a440a7310a4bc539757272740f496ead4ae8e4f6
Alexander Liesenfeld <alexander.liesenfeld@outlook.com>
2021-04-27 20:34:41 +0200

    Replaced structopt with clap.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
-use structopt::StructOpt;
-
 use httpmock::standalone::start_standalone_server;
+use std::env;
 use std::path::PathBuf;
+use clap::{AppSettings, Clap};
 
 /// Holds command line parameters provided by the user.
-#[derive(StructOpt, Debug)]
-pub struct CommandLineParameters {
-    #[structopt(short, long, default_value = "5000")]
+#[derive(Clap, Debug)]
+#[clap(version = "0.6", author = "Alexander Liesenfeld <alexander.liesenfeld@outlook.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct ExecutionParameters {
+    #[clap(short, long, env = "HTTPMOCK_PORT", default_value = "5000")]
     pub port: u16,
-    #[structopt(short, long)]
+    #[clap(short, long, env = "HTTPMOCK_EXPOSE")]
     pub expose: bool,
-    #[structopt(short, long)]
-    pub static_mock_dir: Option<PathBuf>,
-    #[structopt(short, long)]
+    #[clap(short, long, env = "HTTPMOCK_MOCK_FILES_DIR")]
+    pub mock_files_dir: Option<PathBuf>,
+    #[clap(short, long, env = "HTTPMOCK_DISABLE_ACCESS_LOG")]
     pub disable_access_log: bool,
-    #[structopt(short, long, default_value = "250")]
-    pub history_limit: usize,
+    #[clap(short, long, env = "HTTPMOCK_REQUEST_HISTORY_LIMIT", default_value = "100")]
+    pub request_history_limit: usize,
 }
 
 #[tokio::main]
 async fn main() {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("httpmock=info"));
 
-    let params: CommandLineParameters = CommandLineParameters::from_args();
+    let params: ExecutionParameters = ExecutionParameters::parse();
 
     log::info!("██╗  ██╗████████╗████████╗██████╗ ███╗   ███╗ ██████╗  ██████╗██╗  ██╗");
     log::info!("██║  ██║╚══██╔══╝╚══██╔══╝██╔══██╗████╗ ████║██╔═══██╗██╔════╝██║ ██╔╝");
@@ -37,12 +39,14 @@ async fn main() {
         env!("CARGO_PKG_VERSION")
     );
 
+    log::info!("{:?}", params);
+
     start_standalone_server(
         params.port,
         params.expose,
-        params.static_mock_dir,
+        params.mock_files_dir,
         !params.disable_access_log,
-        params.history_limit,
+        params.request_history_limit,
     )
     .await
     .expect("an error occurred during mock server execution");
